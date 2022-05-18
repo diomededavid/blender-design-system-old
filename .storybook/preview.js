8acda704d022d412f6831e3d360f1d2ec421f041
David Diomede <david@d2creativedesign.com>
2022-05-18 16:06:08 -0700

    Add color themes

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -6,6 +6,13 @@ export const parameters = {
     storySort: (a, b) =>
         a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
   },
+  backgrounds: {
+    default: 'light',
+    values: [
+      { name: 'light', value: '#fff' },
+      { name: 'dark', value: '#3b5998' },
+    ],
+  },
   previewTabs: {
     viewMode: 'docs',
     canvas: { hidden: true }
