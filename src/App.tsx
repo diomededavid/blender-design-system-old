682a2db76c68f4954d9e768427394103507ea7a9
s <github@seanhays.com>
2022-05-19 19:12:35 -0700

    adds footer commonent

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Avatar from "./components/Avatar";
+import Footer from "./components/Footer";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Avatar imgUrl='https://robohash.org/bob' size="w-24" altDesc="robot"/>
+      <Avatar imgUrl="https://robohash.org/bob" size="w-24" altDesc="robot" />
+      <Footer description="This is a test by Sean" />
     </div>
   );
 }
