1680957b61e2eb82d25aba3cb375f6fe32ece6e0
David Diomede <david@d2creativedesign.com>
2022-05-29 19:31:58 -0700

    tests navbar look

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Avatar from "./components/Avatar";
 import "./App.css";
 import AvatarGroup from "./components/AvatarGroup";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
+        <Navbar/>
       <AvatarGroup>
         <Avatar imgUrl="https://robohash.org/bob" size="w-12" altDesc="robot" />
         <Avatar imgUrl="https://robohash.org/bob" size="w-12" altDesc="robot" />
