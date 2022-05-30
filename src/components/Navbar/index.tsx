2ee94f7dc92aca7aff0b68e052f73a1edb4cfe9f
David Diomede <david@d2creativedesign.com>
2022-05-29 19:32:15 -0700

    updates navbar content

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -3,18 +3,27 @@ interface NavbarProps {
      * Prop
      */
     prop?: string;
+
 }
 
 export const Navbar = ({}: NavbarProps) => (
-        <div className="navbar bg-white">
-            <div className="flex-none">
-                <button className="btn btn-square btn-ghost">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                         className="inline-block w-5 h-5 stroke-current">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                              d="M4 6h16M4 12h16M4 18h16"/>
-                    </svg>
-                </button>
+        <nav className="navbar bg-white">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost btn-circle">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24"
+                             stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                  d="M4 6h16M4 12h16M4 18h7"/>
+                        </svg>
+                    </label>
+                    <ul tabIndex={0}
+                        className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        <li><a href="https://apple.com/homepage">Homepage</a></li>
+                        <li><a href="https://apple.com/portfolio">Portfolio</a></li>
+                        <li><a href="https://apple.com/about">About</a></li>
+                    </ul>
+                </div>
             </div>
             <div className="flex-1">
                 <a href="https://apple.com" className="btn btn-ghost normal-case text-xl">Navbar</a>
@@ -38,7 +47,7 @@ export const Navbar = ({}: NavbarProps) => (
                     </div>
                 </button>
             </div>
-        </div>
+        </nav>
     );
 
 export default Navbar;
