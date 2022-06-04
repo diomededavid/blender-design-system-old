7e2a4a0be8d7016fc2fbb5666a0611873470d144
David Diomede <david@d2creativedesign.com>
2022-06-04 14:48:26 -0700

    Add navbar slots

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -4,30 +4,25 @@ interface NavbarProps {
      */
     prop?: string;
 
+    /*
+    * Navbar Start section
+    * */
+    navbarStart?: React.ReactNode;
+
+    /*
+* Navbar Start section
+* */
+    navbarMiddle?: React.ReactNode;
+
 }
 
-export const Navbar = ({}: NavbarProps) => (
-        <nav className="navbar bg-white">
-            <div className="navbar-start">
-                <div className="dropdown">
-                    <label tabIndex={0} className="btn btn-ghost btn-circle">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24"
-                             stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                  d="M4 6h16M4 12h16M4 18h7"/>
-                        </svg>
-                    </label>
-                    <ul tabIndex={0}
-                        className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><a href="https://apple.com/homepage">Homepage</a></li>
-                        <li><a href="https://apple.com/portfolio">Portfolio</a></li>
-                        <li><a href="https://apple.com/about">About</a></li>
-                    </ul>
+export const Navbar = ({navbarStart}: NavbarProps) => (
+        <div className="navbar bg-white">
+            { navbarStart &&
+                <div className="navbar-start">
+                    {navbarStart}
                 </div>
-            </div>
-            <div className="flex-1">
-                <a href="https://apple.com" className="btn btn-ghost normal-case text-xl">Navbar</a>
-            </div>
+            }
             <div className="navbar-end">
                 <button className="btn btn-ghost btn-circle">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24"
@@ -47,7 +42,7 @@ export const Navbar = ({}: NavbarProps) => (
                     </div>
                 </button>
             </div>
-        </nav>
+        </div>
     );
 
 export default Navbar;
