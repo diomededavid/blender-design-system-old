ab5dac4a5881449e77d9b4b1a958fd4997fa6abe
David Diomede <david@d2creativedesign.com>
2022-06-06 09:48:37 -0700

    ass navbar component test

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import Avatar from "./components/Avatar";
 import "./App.css";
 import AvatarGroup from "./components/AvatarGroup";
 import Navbar from "./components/Navbar";
+import Menu from "./components/Menu";
 
 function App() {
   return (
     <div className="App">
-        <Navbar navbarStart={<h2>bob</h2>}/>
+        <Navbar navbarStart={<Menu menuTitle="title"/>}/>
       <AvatarGroup>
         <Avatar imgUrl="https://robohash.org/bob" size="w-12" altDesc="robot" />
         <Avatar imgUrl="https://robohash.org/bob" size="w-12" altDesc="robot" />
